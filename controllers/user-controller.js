795cec7921ba4b5fd6556ce4aa5d268a66acc62a
Matt Neal <neal.matt.m@gmail.com>
2022-09-21 20:30:44 -0400

    minor syntax fixes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,11 +1,11 @@
 const User = require('../models/User')
 
 const userController = {
-    getAllUsers((req, res)) {...},
-    getUserById((req, res)) {...},
-    createUser((req, res)) {...},
-    updateUser((req, res)) {...},
-    deleteUser((req, res)) {...}
+    getAllUsers(req, res) {},
+    getUserById(req, res) {},
+    createUser(req, res) {},
+    updateUser(req, res) {},
+    deleteUser(req, res) {}
 }
 
 module.exports = userController
